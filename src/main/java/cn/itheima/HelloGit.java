8fc6aee928f1fa9a3bd0868ad3af190fa809f143
wuyinghui <2033345759@qq.com>
2020-12-04 19:01:16 +0800

    添加文件

---
+++ src/main/java/cn/itheima/HelloGit.java
@@ -0,0 +1,10 @@
+package cn.itheima;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("HelloGit");
+
+	}
+
+}
---
+++ src/main/java/cn/itheima/HelloGit2.java
@@ -0,0 +1,10 @@
+package cn.itheima;
+
+public class HelloGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloGit");
+
+	}
+
+}
