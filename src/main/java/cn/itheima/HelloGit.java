860cd2c14b3e3c15a6650792f8056f79fc95ea4d
wuyinghui <2033345759@qq.com>
2020-12-06 13:44:50 +0800

    我是主干添加

--- src/main/java/cn/itheima/HelloGit.java
+++ src/main/java/cn/itheima/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("HelloGit");
-		System.out.println("我是分支。。。。。。。");
+		System.out.println("我是主干。。。。。。。");
 
 	}
 
